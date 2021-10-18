dd2396054607c4c81e0466b71b68e9ea64cb4f8f
Anusha98s <92158408+Anusha98s@users.noreply.github.com>
2021-10-18 13:27:08 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;namespace Demo_Multithreading1
+{
+    class MyClass
+    {
+        public void Display()
+        {
+            Console.WriteLine("Implementing a thread1"); for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("\n {0}", i);
+            }
+            Console.WriteLine("Thread1 closes");
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text;namespace Demo_Multithreading1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Implementing Multitasking using Multithreading in c#");
+            MyClass History = new MyClass();
+            Console.WriteLine("Main thread starts......");
+            History.Display(); ThreadStart th1 = History.Display; Thread s1 = new Thread(th1); s1.Start();
+            Console.WriteLine("Main thread ends here!!!!");
+            Console.ReadKey();
+        }
+    }
+}
+
+
